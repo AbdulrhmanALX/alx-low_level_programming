e38e35363fb52285c4e74fb2a95abbc1e8573ef8
Abdelrahman <bodyronaldo30@gmail.com>
2023-02-23 21:53:14 +0200

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *print_square - prints square
+  *@n: the length of the square
+  *Return: void
+  */
+void print_square(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
