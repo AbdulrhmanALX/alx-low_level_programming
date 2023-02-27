31df5e517b0e71937361927899c5b1e2b12c2b49
Abdulrhman <bodycr@yahoo.com>
2023-02-27 14:33:41 +0200

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - swap 2 integers
+ * @a: param pointer
+ * @b: param pointer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp
+	
+	tmp = *a;
+	*a = *b;
+	*b = tmp; 
+}
