2a99ab6e4a6f7702a2a8ff53f55580e3474f25af
Abdulrhman <bodycr@yahoo.com>
2023-02-27 14:57:30 +0200

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+/**
+ * int _strlen -  returns the length of a string.
+ * @s: param charachter
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (; *s; *s++)
+		a++;
+	return (a);
+}
